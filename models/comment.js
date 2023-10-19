0508b98024073a38e9ad71e40240f112370114e7
Robert <rcastor0000@gmail.com>
2023-10-19 15:48:54 -0500

    save

--- models/comment.js
+++ models/comment.js
@@ -2,10 +2,10 @@ const { Schema } = require('mongoose')
 
 const commentSchema = new Schema(
     {
-        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        author: { type: Schema.Types.ObjectId, ref: 'users', required: true },
         title: { type: String, required: true },
         text_content: { type: String, required: true },
-        post_id: { type: Schema.Types.ObjectId, ref: 'Post', required: true } 
+        post_id: { type: Schema.Types.ObjectId, ref: 'posts', required: true } 
     },
     { timestamps: true }
 )
